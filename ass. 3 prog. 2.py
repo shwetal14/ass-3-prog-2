cd5dbc13efcbc7d0a239d749c8b49bc55538d605
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2021-12-30 00:19:57 +0530

    Add files via upload

---
+++ ass. 3 prog. 2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[24]:
+
+
+
+
+def reverse_string(my_string):
+  reversed_string = ''
+  for character in my_string[::-1]:
+      reversed_string += character
+  return reversed_string
+
+my_string = input('enter a string:')
+print('reversed string :', reverse_string(my_string))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
